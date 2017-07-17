b7ba0a47180e1c4e6491c3a00ae59ebc96fb1eda
Titus Wormer <tituswormer@gmail.com>
2017-07-17 11:57:19 -0400

    Don’t expect iTerm in CIs

--- test.js
+++ test.js
@@ -2,6 +2,9 @@ import test from 'ava';
 import vfile from 'vfile';
 import m from './';
 
+/* https://github.com/sindresorhus/eslint-formatter-pretty/blob/159b30a/index.js#L90-L93 */
+const cwd = process.env.CI ? '' : '\u001b]50;CurrentDir=/Users/tilde/projects/oss/vfile-reporter-pretty\u0007';
+
 test(t => {
 	const file = vfile({path: '~/example.md'});
 
@@ -16,7 +19,7 @@ test(t => {
 		m([file]),
 		[
 			'',
-			'\u001b]50;CurrentDir=/Users/tilde/projects/oss/vfile-reporter-pretty\u0007  \u001b[4m~/example.md\u001b[8m\u001b[2m\u001b[90m:1:8\u001b[39m\u001b[22m\u001b[28m\u001b[24m',
+			cwd + '  \u001b[4m~/example.md\u001b[8m\u001b[2m\u001b[90m:1:8\u001b[39m\u001b[22m\u001b[28m\u001b[24m',
 			'  \u001b[33m⚠\u001b[39m  \u001b[2m1\u001b[90m:\u001b[39m8\u001b[22m  \u001b[1mbraavo\u001b[22m is misspelt; did you mean \u001b[1mbravo\u001b[22m?  ',
 			'  \u001b[33m⚠\u001b[39m  \u001b[2m2\u001b[90m:\u001b[39m1\u001b[22m  This is perfect                          ',
 			'  \u001b[31m✖\u001b[39m  \u001b[2m3\u001b[90m:\u001b[39m5\u001b[22m  This is horrible                         ',
