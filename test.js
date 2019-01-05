17f9e987cd2b8b3529629bd260b24e18fb7db572
Titus <tituswormer@gmail.com>
2019-01-05 22:27:02 +0100

    Add support for tests on Windows
    Closes GH-2.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>

--- test.js
+++ test.js
@@ -1,12 +1,16 @@
+import path from 'path'
 import test from 'ava'
 import vfile from 'vfile'
+import symbols from 'log-symbols'
+import chalk from 'chalk'
 import m from '.'
 
 // https://github.com/sindresorhus/eslint-formatter-pretty/blob/159b30a/index.js#L90-L93
 const cwd = process.env.CI ? '' : `\u001B]50;CurrentDir=${process.cwd()}\u0007`
 
 test(t => {
-  const file = vfile({path: '~/example.md'})
+  const fp = path.join('~', 'example.md')
+  const file = vfile({path: fp})
 
   file.info('This is perfect', {line: 5, column: 3}, 'alpha:bravo')
 
@@ -30,14 +34,26 @@ test(t => {
     m([file]),
     [
       '',
-      cwd +
-        '  \u001B[4m~/example.md\u001B[24m\u001B[8m\u001B[2m\u001B[90m:5:3\u001B[39m\u001B[22m\u001B[28m',
-      '  \u001B[33m⚠\u001B[39m  \u001B[2m3\u001B[90m:\u001B[39m5\u001B[22m  This should be fixed  ',
-      '  \u001B[33m⚠\u001B[39m  \u001B[2m5\u001B[90m:\u001B[39m3\u001B[22m  This is perfect       \u001B[2malpha:bravo\u001B[22m',
-      '  \u001B[31m✖\u001B[39m  \u001B[2m2\u001B[90m:\u001B[39m1\u001B[22m  This is horrible      ',
+      cwd + '  ' + chalk.underline(fp) + chalk.hidden.dim.gray(':5:3'),
+      '  ' +
+        symbols.warning +
+        '  ' +
+        chalk.dim('3' + chalk.gray(':') + '5') +
+        '  This should be fixed  ',
+      '  ' +
+        symbols.warning +
+        '  ' +
+        chalk.dim('5' + chalk.gray(':') + '3') +
+        '  This is perfect       ' +
+        chalk.dim('alpha:bravo'),
+      '  ' +
+        symbols.error +
+        '  ' +
+        chalk.dim('2' + chalk.gray(':') + '1') +
+        '  This is horrible      ',
       '',
-      '  \u001B[33m2 warnings\u001B[39m',
-      '  \u001B[31m1 error\u001B[39m',
+      '  ' + chalk.yellow('2 warnings'),
+      '  ' + chalk.red('1 error'),
       ''
     ].join('\n')
   )
