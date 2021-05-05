aa124e6524b9eb6ef5a126dba13cae3eb7be716f
Titus Wormer <tituswormer@gmail.com>
2021-05-05 21:22:08 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+/**
+ * @typedef {import('vfile').VFile} VFile
+ */
+
 import {toESLint} from 'vfile-to-eslint'
 import pretty from 'eslint-formatter-pretty'
 
+/**
+ * @param {Array.<VFile>} vfiles
+ * @returns {string}
+ */
 export function reporterPretty(vfiles) {
   return pretty(toESLint(vfiles))
 }
