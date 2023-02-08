2d64d2ecb2c5a042bbf2327c4014d48b5ee48297
Titus Wormer <tituswormer@gmail.com>
2023-02-08 10:16:14 +0100

    Refactor code-style
    *   Add more docs to JSDoc

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ import {toESLint} from 'vfile-to-eslint'
 import pretty from 'eslint-formatter-pretty'
 
 /**
+ * Create a pretty report from files.
+ *
  * @param {Array<VFile>} vfiles
+ *   Files to report.
  * @returns {string}
+ *   Pretty eport.
  */
 export function reporterPretty(vfiles) {
   // @ts-ignore: types for `eslint-formatter-pretty` are out of date.
